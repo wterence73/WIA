7b6acde7b952ed39def2d31c12740066ce5ad870
Terence Williams <wterence73@gmail.com>
2015-10-24 21:56:45 -0400

    Project Update

--- Williams_Terence_WIA_The_Project/js/main.js
+++ Williams_Terence_WIA_The_Project/js/main.js
@@ -1,7 +1,48 @@
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+window.onload = function() {
+    
+ var theCanvas = document.getElementById('Canvas1');
+    
+    if (theCanvas && theCanvas.getContext) {
+        var ctx = theCanvas.getContext("2d");
+        
+        if(ctx) {
+            var myText="Welcome to Disney!";
+
+            ctx.font="21pt Comic Sans";
+            ctx.fillStyle="red";
+            ctx.fillText(myText,40,80);
+            
+        }
+        
+    }   
+    
+    var theCanvas = document.getElementById('Canvas2');
+    
+   		 if (theCanvas && theCanvas.getContext) {
+        	var ctx = theCanvas.getContext("2d");
+        
+       	 	if(ctx) {
+       	 			var srcImg = document.getElementById("img2");
+       	 			
+       	 			ctx.drawImage(srcImg, 0, 0, 700, 400);
+       	 			
+       	 		}
+       	 	}
+    
+    var theCanvas = document.getElementById('Canvas2');
+    
+    if (theCanvas && theCanvas.getContext) {
+        var ctx = theCanvas.getContext("2d");
+        
+        if(ctx) {
+            var myText="We hope to see you soon!";
+
+            ctx.font="36pt Comic Sans";
+            ctx.fillStyle="red";
+            ctx.fillText(myText,110,450);
+            
+        }
+        
+    }   
+    
 }
